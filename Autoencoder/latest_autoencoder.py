68406080178264948f959c8f602b6bae1bbf6480
daviddowdell <david.t.dowdell@ou.edu>
2021-04-02 12:03:36 -0500

    Added sequential nlpca model

---
+++ Autoencoder/latest_autoencoder.py
@@ -0,0 +1,109 @@
+#NEw autoencoder
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import random
+import math
+
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+from sklearn.model_selection import train_test_split
+from tensorflow.keras import layers, losses
+from tensorflow.keras.datasets import fashion_mnist
+from tensorflow.keras.models import Model
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+
+#Classic Sparse Autoencoder model for NLPCA
+class myAutoencoder(Model):
+    def __init__(self, visible_dim=2, latent_dim=1, mapping_layers=6, activation='sigmoid'):
+        super(myAutoencoder, self).__init__()
+        self.latent_dim = latent_dim
+        self.visible_dim=visible_dim
+        self.mapping_layers=mapping_layers
+        
+        #Encoder
+        self.encoder = tf.keras.Sequential([
+            #Flatten just in case
+            layers.Flatten(),
+            #First hidden layer
+            layers.Dense(mapping_layers, activation=activation),
+            #Bottleneck layer
+            layers.Dense(latent_dim, activation='linear'),
+            ])
+        #Decoder
+        self.decoder = tf.keras.Sequential([
+            #Third hidden layer
+            layers.Dense(mapping_layers, activation=activation),
+            #Output layer
+            layers.Dense(visible_dim, activation='linear'),
+            #Put back into 28x28
+            layers.Reshape((28,28))
+            ])
+    def call(self, x):
+        endoced = self.encoder(x)
+        decoded = self.decoder(endoced)
+        return decoded
+
+#Sequential Autoencoder model for NLPCA
+class sequentialAutoencoder():
+    def __init__(self, visible_dim=2, latent_dim=1, mapping_layers=[6], activation='sigmoid'):
+        self.coders = []
+        for i in range(latent_dim):
+            if type(mapping_layers) == type(1) :
+                ml = mapping_layers
+            else: ml = mapping_layers[i]    
+            #Create list of autoencoders
+            self.coders.append(myAutoencoder(visible_dim=visible_dim, latent_dim=1,
+                                             mapping_layers=ml,
+                                             activation=activation))
+            self.coders[i].compile(optimizer=tf.keras.optimizers.Adam(lr=0.1),
+                                   loss=losses.MeanSquaredError())
+
+    def train(self, data, epochs=1):
+        for ac in self.coders:
+            ac.fit(data, data, epochs=epochs, shuffle=True, verbose=2,
+                   validation_data=(data,data))
+            data_prime = ac.call(data)
+            data = data - data_prime
+            #print(data)
+    def call(self, data):
+        final_data = data - data #so it's all zeroes
+        for ac in self.coders:
+            data_prime = ac.call(data)
+            final_data = final_data + data_prime
+            data = data - data_prime
+        return final_data
+
+def lookatclothes():
+    (x_train, _), (x_test, _) = fashion_mnist.load_data()
+    x_train = x_train.astype('float32') / 255.
+    x_test = x_test.astype('float32') / 255. # Converts uint8 to float32 between 0 and 1
+    print (x_train.shape)
+    print(type(x_train))
+    print (x_test.shape)
+    sqa = sequentialAutoencoder(visible_dim=784, latent_dim=8, mapping_layers=100)
+    sqa.train(x_train, epochs=2)
+
+    recoded = sqa.call(x_test).numpy()
+    n = 10
+    plt.figure(figsize=(20, 4))
+    for i in range(n):
+      # display original
+      ax = plt.subplot(2, n, i + 1)
+      plt.imshow(x_test[i])
+      plt.title("original")
+      plt.gray()
+      ax.get_xaxis().set_visible(False)
+      ax.get_yaxis().set_visible(False)
+    
+      # display reconstruction
+      ax = plt.subplot(2, n, i + 1 + n)
+      plt.imshow(recoded[i])
+      plt.title("reconstructed")
+      plt.gray()
+      ax.get_xaxis().set_visible(False)
+      ax.get_yaxis().set_visible(False)
+    plt.show()
+
+lookatclothes()
